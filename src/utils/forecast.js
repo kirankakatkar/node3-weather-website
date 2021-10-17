5bbeb23a833940ab9fd5ee8afd3e4bd80dfd0a70
kiran kakatkar <kiran.kakatkar@harbingergroup.com>
2021-10-17 20:09:12 +0530

    updated the forecast message

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -10,7 +10,8 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, `${body.current.weather_descriptions[0]}. It is currently ${body.current.temperature} degrees out. It feels like ${body.current.feelslike} degrees out.`)
+            // callback(undefined, `${body.current.weather_descriptions[0]}. It is currently ${body.current.temperature} degrees out. It feels like ${body.current.feelslike} degrees out.`)
+            callback(undefined, `${body.current.weather_descriptions[0]}. The current temperature is ${body.current.temperature} degrees out, but it feels like ${body.current.feelslike} degrees out. The humidity is ${body.current.humidity}`)
         }
     })
 }
